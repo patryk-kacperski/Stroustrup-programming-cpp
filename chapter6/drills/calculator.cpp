58daaad6b9a4c687922b8bc52b1b7e02610a4f8b
Patryk Kacperski <patryk.kacperski0@gmail.com>
2018-06-26 15:38:27 +0200

    Calculator v6.2. Operands unlimited (terminating with 'x'). Added multiplication and division

--- chapter6/drills/calculator.cpp
+++ chapter6/drills/calculator.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Please enter expression (we can handle + and -): ";
+	std::cout << "Please enter expression (we can handle +, -, *, /)\n";
+	std::cout << "add an x to end expression (e.g., 1+2*3x): ";
 	int lval = 0;
 	int rval;
-	char op;
-	int res;
-	std::cin >> lval >> op >> rval;
-
-	if (op == '+') {
-		res = lval + rval;
-	} else if (op == '-') {
-		res = lval - rval;
+	std::cin >> lval;
+	if (!std::cin) {
+		std::cerr << "no first operand\n";
+		return 1;
 	}
 
-	std::cout << "Result: " << res << '\n';
-	return 0;
+	for (char op; std::cin >> op;) {
+		if (op != 'x') {
+			std::cin >> rval;
+		}
+		if (!std::cin) {
+			std::cerr << "no second operand\n";
+			return 1;
+		}
+		switch (op) {
+		case '+':
+			lval += rval;
+			break;
+		case '-':
+			lval -= rval;
+			break;
+		case '*':
+			lval *= rval;
+			break;
+		case '/':
+			lval /= rval;
+			break;
+		default:
+			std::cout << "Result: " << lval << '\n';
+			return 0;
+		}
+		
+	}
+	std::cerr << "bad expression\n";
+	return 1;
 }
